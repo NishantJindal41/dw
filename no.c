279312643a5261599ef76436b60e4f39672ea765
NishantJindal41 <44042222+NishantJindal41@users.noreply.github.com>
2018-10-12 03:06:02 -0700

    Create no.c

---
+++ no.c
@@ -0,0 +1,12 @@
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%2==0)
+    printf("EVEN");
+    else if(a%2==1)
+    printf("ODD");
+    else
+    printf("INVALID INPUT");
+    return 0;
+}
